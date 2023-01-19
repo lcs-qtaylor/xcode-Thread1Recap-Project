2f1c54f9a6f7a140c16c8129a12e1e7bfae0ef04
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-01-19 12:31:06 -0500

    Working on Temp Convert havnt got it working yet or the ui done

--- AreaAndPerimeterCalculator/AreaAndPerimeterCalculator/Views/TempConvert.swift
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculator/Views/TempConvert.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct TempConvert: View {
+    @State var Celcius: Double = 1
+    @State var Fahrenheit: Double = 33.8
+    
+var celcius: Double {
+        return fahrenheit + 32 * 1.8
+    }
+var fahrenheit: Double {
+        return (celcius - 32) * 0.5556
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Stepper("Celcius",
+                value: $celcius,
+                in: 5.0...30.0,
+                step: 0.5)
+        
+        HStack {
+            Spacer()
+            Text("21.0 °C")
+                .font(.largeTitle)
+            Spacer()
+        }
+        Stepper("Fahrenheit",
+                value: $fahrenheit,
+                in: 5.0...30.0,
+                step: 0.5)
+        
+        HStack {
+            Spacer()
+            Text("21.0 °C")
+                .font(.largeTitle)
+            Spacer()
+        }
     }
 }
 
