66c513d68eda4ec23084cc02b4305ee8daf81839
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-02-04 11:17:09 -0500

    Working on the Computed Properties.

--- WillitosStockCalculator/Views/CalculatorView.swift
+++ WillitosStockCalculator/Views/CalculatorView.swift
@@ -14,8 +14,54 @@ struct CalculatorView: View {
     @State var stockPercent = ""
     @Binding var history: [Result]
     // MARK: Computed properties
-   
+    var stockPrice: Double? {
+        
+        guard let priceAsDouble = Double(buyStockPrice) else {
+           
+            return nil
+        }
+        return priceAsDouble
+    }
+    var quantity: Double? {
+        
+        guard let quantityAsDouble = Double(Quantity) else {
+           
+            return nil
+        }
+        return quantityAsDouble
+    }
+    //
+    var totalReturn: Double? {
+        
+        // Is the bill amount nil, or an actual numeric value?
+        guard let amount = stockPrice else {
+            return nil
+        }
+        
+        // Calculate the tip dollar amount
+        let  = amount * quantity
+        
+        // Calculate bill total, including the tip
+        let billTotal = amount + tipAmount
+        
+        // Return total bill
+        return billTotal
+    }
     
+    
+    //
+    var totalReturnFormatted: String {
+        
+        // Could the total with tip be calculated?
+        // Or did that fail because the bill amount input was nonsense?
+        guard let total = totalReturn else {
+            return "Cannot be calculated..."
+        }
+        
+        // It could be calculated, so format it nicely
+        return total.formatted(.number.precision(.fractionLength(2)))
+        
+    }
     var body: some View {
         NavigationView {
             
@@ -63,19 +109,19 @@ struct CalculatorView: View {
                         .bold()
                     Stepper("",
                             value: $stockPercent,
-                            in: 0.0...100.0,
+                            in: 0...100,
                             step: 1.0)
                     .padding(.trailing, 140)
                     Spacer()
                 }
                 HStack{
                     Slider(value: $stockPercent,
-                           in: -100.0...100.0,
+                           in: -100...100,
                            label: {
                         Text("Opacity")
                     },
                            minimumValueLabel: {
-                        Text("0")
+                        Text("-100")
                     },
                            maximumValueLabel: {
                         Text("100")
