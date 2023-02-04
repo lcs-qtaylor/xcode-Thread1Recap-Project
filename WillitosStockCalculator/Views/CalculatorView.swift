d2380762cb0ac2dcaaa4aa73f752ab3b510e9db8
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-02-04 13:18:53 -0500

    Finished Computed Properties (Hopefully)

--- WillitosStockCalculator/Views/CalculatorView.swift
+++ WillitosStockCalculator/Views/CalculatorView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct CalculatorView: View {
     // MARK: Stored properties
     @State var buyStockPrice = ""
-    @State var Quantity = ""
-    @State var stockPercent = ""
-    @State var Return = ""
+    let stockPercent = [0...1000]
+    @State var selectedPercent = 0
+    @State var Quantity = 0
     @Binding var history: [Result]
     // MARK: Computed properties
     var stockPrice: Double? {
@@ -23,52 +23,68 @@ struct CalculatorView: View {
         }
         return priceAsDouble
     }
-    var quantity: Double? {
-        
-        guard let quantityAsDouble = Double(Quantity) else {
-           
-            return nil
-        }
-        return quantityAsDouble
-    }
-    var percent: Double? {
-        
-        guard let percentAsDouble = Double(stockPercent) else {
-           
-            return nil
-        }
-        return percentAsDouble
+//    var quantity: Double? {
+//
+//        guard let quantityAsDouble = Double(Quantity) else {
+//
+//            return nil
+//        }
+//        return quantityAsDouble
+//    }
+//    var percent: Double? {
+//
+//        guard let percentAsDouble = Double(stockPercent) else {
+//
+//            return nil
+//        }
+//        return percentAsDouble
+//    }
+    var quantity: Double {
+        return Double(Quantity)
     }
     
-    //
-    var totalReturn: Double? {
-        
-        // Is the bill amount nil, or an actual numeric value?
+    var totalInvestment: Double? {
         guard let amount = stockPrice else {
             return nil
         }
+        let investment = amount * quantity
         
-      
-        let investment = amount * quantity!
-        
-        // Calculate bill total, including the tip
-        let gain = investment * percent!
-        
-        // Return total bill
-        return gain
+        return investment
+    }
+    var percent: Double {
+        return Double(selectedPercent) / 100
     }
     
+    var totalReturn: Double? {
+        guard let investment = totalInvestment else {
+            return nil
+        }
+        
+        let gain = percent * investment
+        
+        let Return = gain + investment
+        
+        return Return
+    }
     
-    //
     var totalReturnFormatted: String {
         
-
         guard let total = totalReturn else {
             return "Cannot be calculated..."
         }
         
         // It could be calculated, so format it nicely
         return total.formatted(.number.precision(.fractionLength(2)))
+        
+    }
+    var totalInvestmentFormatted: String {
+        
+        guard let total2 = totalInvestment else {
+            return "Cannot be calculated..."
+        }
+        
+        // It could be calculated, so format it nicely
+        return total2.formatted(.number.precision(.fractionLength(2)))
         
     }
     var body: some View {
@@ -90,50 +106,63 @@ struct CalculatorView: View {
                     }
                     .padding(.horizontal)
                 }
-                
-                Group{
+                Group {
                     HStack {
                         Text("Quantity")
                         Spacer()
                     }
-                    .padding(.horizontal)
-                    HStack(spacing: 5){
-                        Text("")
-                        
-                        TextField("0.0", text: $Quantity)
+                    HStack{
                         
                     }
-                    .padding(.horizontal)
-                    
-                    
                 }
-                Group{
-                    HStack {
-                        Text("Quantity")
+                Group {
+                    HStack{
+                    HStack{
+                        Text("$")
+                        
+                        Text(totalReturnFormatted)
+                        
                         Spacer()
                     }
-                    .padding(.horizontal)
-                    HStack(spacing: 5){
-                        Text("%")
-                        
-                        TextField("0.0", text: $stockPercent)
-                        
-                    }
-                    .padding(.horizontal)
-                    
-                    
                 }
                 
+                
+                Button(action: {
+                    
+                    // Create a string version of the bill amount
+                    guard let amount = stockPrice else {
+                        return
+                    }
+                    let amountFormatted = String( amount.formatted(.number.precision(.fractionLength(2))))
+                    
+                    // Create a string version of the percentage
+                    let percentage = String(selectedPercent)
+                    
+                    // Create a string version of the people count
+                    let quantity = String(Quantity)
+                    
+                    // Create the prior result, all put together into an instance of Result
+                    let priorResult = Result(ProvidedStockPrice: amountFormatted,
+                                             Shares: quantity,
+                                             stockPercent: percentage,
+                                             Return: totalReturnFormatted)
+                    
+                    // Save the prior result to the history
+                    history.append(priorResult)
+                    
+                }, label: {
+                    Text("Save")
+                        .font(.headline.smallCaps())
+                })
+                .buttonStyle(.bordered)
+                
+                Spacer()
             }
+            .navigationTitle("Broker Calculator")
             
         }
-       
-        .navigationTitle("Broker Calculator")
-        
-        
     }
 }
-
 struct CalculatorView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
